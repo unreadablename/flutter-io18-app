0c945c3e82c0e6dac9e2d5723f756355faaf9fc8
Oleksandr Pidlisnyi <oleksandr.pidlisnyi@gmail.com>
2019-01-22 17:29:21 +0200

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter I/O',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
